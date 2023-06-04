e4040cf83fa0d3292123a2c75bcd170d0037bc20
vincentDubresson <vincent.dubresson@orange.fr>
2023-06-04 16:22:41 +0200

    Add Sass

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
+const path = require('path');
 
 /** 
  * es-lint error is due to vscode extension
  * @type {import('next').NextConfig}
  * */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
